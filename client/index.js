d2f25d67b76548d953284faa87a57df13ece40ed
josecarlos06 <l181130425@gamadero.tecnm.mx>
2022-10-05 21:25:22 -0500

    agregar precargador de css

--- client/index.js
+++ client/index.js
@@ -1,2 +1,6 @@
+import './stylesheets/style.css'
+
 const texto = document.querySelector('#texto');
-texto.textContent="Texto ingresado desde Javascript 😈"
+texto.textContent="Texto ingresado desde Javascript 😈";
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const path = require('path');
 // Exportamos un objeto de configuración
 // que sera usado por webpack
 module.exports = {
+  mode: 'production',
   // 1. El archivo de entrada o indexador
   entry: "./client/index.js",
   // 2. Especificar el archivo de salida
@@ -14,12 +15,32 @@ module.exports = {
     filename: "bundle.js"
   },
   // 3. Configurando el servidor de desarrollo
-  devServer: {
-    // 3.1 Folder de archivos estáticos
-    static: path.join(__dirname, "public"),
-    // 3.2 Puerto del servidor de desarrollo
-    port: 3410,
-    // 3.3 Definiendo el host
-    host: "localhost"
-  }
+  module : {
+    rules : [
+       {
+          // expresiones regular
+          test : /\.js$/,
+          exclude : / (node_modules | bower_components) /,
+          use : [
+             {
+                loader : "babel-loader",
+                options : {
+                   presets : [ 
+                      [
+                         "@babel/preset-env" ,
+                         {
+                            "modules" : false,
+                            "useBuiltIns" : "usage",
+                            "targets" : " > 0.25%, not dead",
+                            "corejs" : 3
+ 
+                         }
+                      ]
+                   ]
+                }
+             }
+          ]
+       }
+    ]
+   }
 }
