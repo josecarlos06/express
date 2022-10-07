235927e3eaf038b59caffba9aa71548eda85a25a
josecarlos06 <l181130425@gamadero.tecnm.mx>
2022-10-06 20:09:22 -0500

    cd

--- server/app.js
+++ server/app.js
@@ -1,33 +1,58 @@
+// Biblioteca de 3ros para manejar errores http
+// ES5: var createError = require('http-errors');
+// ES6 👇
 import createError from 'http-errors';
+// El framework express
 import express from 'express';
+// Biblioteca del nucleo de node que sirve para
+// administrar rutas
 import path from 'path';
+// Biblioteca externa que sirve para administrar
+// cookies
 import cookieParser from 'cookie-parser';
+// Biblioteca que registra en consola
+// solicitudes del cliente
 import logger from 'morgan';
-import indexRouter from './routes/index';
-import usersRouter from './routes/users';
 
+// Definición de rutas
+import indexRouter from "./routes/index";
+import usersRouter from "./routes/users";
+
+// Creando una instancia de express
 const app = express();
 
 // view engine setup
+// Configura el motor de plantillas
+// 1. Establecer donde estarán las plantillas
+// (Vistas -> Views)
+// app.set("<nombre de la var>", <valor>)
 app.set('views', path.join(__dirname, 'views'));
+// Establezco que motor precargado usare
 app.set('view engine', 'hbs');
 
-app.use(logger('dev'));
+// Establezco Middelware
+app.use((logger('dev')));
+// Middleware para parsear a json la peticion
 app.use(express.json());
+// Decodificar la url
 app.use(express.urlencoded({ extended: false }));
+// Parsear cookies
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname,'../','public')));
+// Servidor de archivos estáticos
+app.use(express.static(path.join(__dirname,'..', 'public')));
 
+// Registro Rutas
 app.use('/', indexRouter);
+app.use('/index', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use((req, res, next) =>{
+app.use((req, res, next)=> {
   next(createError(404));
 });
 
 // error handler
-app.use((err, req, res, next) =>{
+app.use((err, req, res, next)=> {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -37,4 +62,8 @@ app.use((err, req, res, next) =>{
   res.render('error');
 });
 
+// Exportando la instancia del server "app"
+// ES5 👇
+// module.exports = app;
+// ES6 👇
 export default app;
