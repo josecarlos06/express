acff929a9bf248a7451c3dd4e8dcad1aa9ead925
josecarlos06 <l181130425@gamadero.tecnm.mx>
2022-10-06 20:28:20 -0500

    cambios

--- server/bin/www.js
+++ server/bin/www.js
@@ -7,15 +7,9 @@ import http from 'http';
 const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
-/**
- * Create HTTP server.
- */
 
 const server = http.createServer(app);
 
-/**
- * Listen on provided port, on all network interfaces.
- */
 
 server.listen(port);
 server.on('error', onError);
