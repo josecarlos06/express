007e916cddb141a96c33bdba4da94e733c0f5de0
josecarlos06 <l181130425@gamadero.tecnm.mx>
2022-11-07 08:08:15 -0600

    agregar materialize

--- client/index.js
+++ client/index.js
@@ -1,4 +1,7 @@
 // Cargado estilos
 import './stylesheet/style.css';
+import 'materialize-css/dist/css/materialize.css';
+import 'materialize-css/dist/js/materialize';
 
+M.AutoInit();
 console.log('Webpack Working Middleware!!! 📦');
